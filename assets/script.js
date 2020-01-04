048ebcb166118b750ac219dbd3ba0a513a7bb682
Noisey Turtle <bobbyoldani2@gmail.com>
2020-01-03 16:59:28 -0800

    Quiz iterates though questions, takes time off for misses and displays correct/wrong

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,11 @@
-
+let body = document.querySelector("body");
 let countEl = document.querySelector("#count");
 let quizEl = document.querySelector("#quiz");
 let h3El = document.querySelector("h3");
+let buttons = document.querySelector("#buttons");
+let i = 0;
 
-
-
-
-
+console.log(questions.title)
 
 
 function quizFun(){
@@ -25,13 +24,154 @@ function quizFun(){
     startEl.addEventListener("click", function() {
         let timeLeft = 75;
 
+        h3El.textContent = "";
+        rulesEl.remove();
+        startEl.remove();
+
         let timeInterval = setInterval(function(){
             countEl.textContent = timeLeft;
             timeLeft--;
+               
+
         }, 1000)
 
+        startQuiz();
+           
+        function startQuiz() {
+        
+        console.log(i);
 
+        let questionEl = document.createElement("h3");
+        let buttons = document.createElement("div")
+        let button1 = document.createElement("button");
+        let button2 = document.createElement("button");
+        let button3 = document.createElement("button");
+        let button4 = document.createElement("button");
+        let answerDiv = document.createElement("div")
+        let answerCheck = document.createElement("h5")
+
+        buttons.setAttribute("id", "buttons");
+        answerDiv.setAttribute("id", "answercheck")
+
+        quizEl.appendChild(questionEl);
+        quizEl.appendChild(buttons);
+        buttons.appendChild(button1);
+        buttons.appendChild(button2);
+        buttons.appendChild(button3);
+        buttons.appendChild(button4);
+
+        questionEl.innerHTML = questions[i].title;
+        button1.textContent = questions[i].choices[0];
+        button2.textContent = questions[i].choices[1];
+        button3.textContent = questions[i].choices[2];
+        button4.textContent = questions[i].choices[3];
+
+        buttons.addEventListener("click", function(){
+            if (i <= questions.length - 2) {
+                if (button1 !== questions[i].answer)
+                quizEl.removeChild(questionEl);
+                quizEl.removeChild(buttons);
+                i++;
+            startQuiz();
+            } else {
+                i++;
+                quizEl.removeChild(questionEl);
+                quizEl.removeChild(buttons);
+                clearInterval(timeInterval);
+            }
+            
+        })
+
+        button1.addEventListener("click", function(){
+            if (button1.textContent !== questions[i].answer) {
+                timeLeft -= 15;
+                body.append(answerDiv);
+                answerDiv.append(answerCheck);
+                answerCheck.textContent = "Wrong!";
+                setTimeout(function() {
+                    answerCheck.textContent = "";
+                }, 1500);
+
+            } else {
+                body.append(answerDiv);
+                answerDiv.append(answerCheck);
+                answerCheck.textContent = "Correct!";
+                setTimeout(function() {
+                    answerCheck.textContent = "";
+                }, 1500);
+            }
+        })
+
+        button2.addEventListener("click", function(){
+            if (button2.textContent !== questions[i].answer) {
+                timeLeft -= 15;
+                body.append(answerDiv);
+                answerDiv.append(answerCheck);
+                answerCheck.textContent = "Wrong!";
+                setTimeout(function() {
+                    answerCheck.textContent = "";
+                }, 1500);
+
+            } else {
+                body.append(answerDiv);
+                answerDiv.append(answerCheck);
+                answerCheck.textContent = "Correct!";
+                setTimeout(function() {
+                    answerCheck.textContent = "";
+                }, 1500);
+            }
+        })
+
+        button3.addEventListener("click", function(){
+            if (button3.textContent !== questions[i].answer) {
+                timeLeft -= 15;
+                body.append(answerDiv);
+                answerDiv.append(answerCheck);
+                answerCheck.textContent = "Wrong!";
+                setTimeout(function() {
+                    answerCheck.textContent = "";
+                }, 1500);
+
+            } else {
+                body.append(answerDiv);
+                answerDiv.append(answerCheck);
+                answerCheck.textContent = "Correct!";
+                setTimeout(function() {
+                    answerCheck.textContent = "";
+                }, 1500);
+            }
+        })
+
+        button4.addEventListener("click", function(){
+            if (button4.textContent !== questions[i].answer) {
+                timeLeft -= 15;
+                body.append(answerDiv);
+                answerDiv.append(answerCheck);
+                answerCheck.textContent = "Wrong!";
+                setTimeout(function() {
+                    answerCheck.textContent = "";
+                }, 1500);
+
+            } else {
+                body.append(answerDiv);
+                answerDiv.append(answerCheck);
+                answerCheck.textContent = "Correct!";
+                setTimeout(function() {
+                    answerCheck.textContent = "";
+                }, 1500);
+            }
+        })
+
+        
+        
+        }
+
+        
     })
+
+
+
 }
 
 quizFun();
+
