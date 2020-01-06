d774a77bb9f4b13a6d814a7a5d6ad79b0e6dc339
Noisey Turtle <bobbyoldani2@gmail.com>
2020-01-05 19:03:37 -0800

    inputs storing to local storage now

--- assets/script.js
+++ assets/script.js
@@ -4,9 +4,16 @@ let quizEl = document.querySelector("#quiz");
 let h3El = document.querySelector("h3");
 let buttons = document.querySelector("#buttons");
 let i = 0;
+let timeLeft = 75
 
-console.log(questions.title)
+//console.log(questions.title)
 
+// if(timeLeft <= 0) {                                                 //I cant figure out how to get this to work. Most likely because I set this up like a mad man...
+//     quizEl.removeChild(questionEl);
+//     quizEl.removeChild(buttons);
+//     highScore(); //call highScore() that adds input box and such...
+//     clearInterval(timeInterval);
+// }
 
 function quizFun(){
     let rulesEl = document.createElement("p");
@@ -22,7 +29,6 @@ function quizFun(){
 
 
     startEl.addEventListener("click", function() {
-        let timeLeft = 75;
 
         h3El.textContent = "";
         rulesEl.remove();
@@ -31,15 +37,17 @@ function quizFun(){
         let timeInterval = setInterval(function(){
             countEl.textContent = timeLeft;
             timeLeft--;
-               
+               console.log(timeLeft);
 
         }, 1000)
+        
 
         startQuiz();
            
         function startQuiz() {
         
-        console.log(i);
+        //console.log(i);
+        
 
         let questionEl = document.createElement("h3");
         let buttons = document.createElement("div")
@@ -49,7 +57,8 @@ function quizFun(){
         let button4 = document.createElement("button");
         let answerDiv = document.createElement("div")
         let answerCheck = document.createElement("h5")
-
+        
+        answerCheck.style.textAlign = "center";
         buttons.setAttribute("id", "buttons");
         answerDiv.setAttribute("id", "answercheck")
 
@@ -66,21 +75,33 @@ function quizFun(){
         button3.textContent = questions[i].choices[2];
         button4.textContent = questions[i].choices[3];
 
+        
         buttons.addEventListener("click", function(){
-            if (i <= questions.length - 2) {
+            if (i <= questions.length - 2) {                      
                 if (button1 !== questions[i].answer)
                 quizEl.removeChild(questionEl);
                 quizEl.removeChild(buttons);
                 i++;
-            startQuiz();
+                startQuiz();
             } else {
                 i++;
                 quizEl.removeChild(questionEl);
                 quizEl.removeChild(buttons);
+                
+
+                highScore(); //call highScore() that adds input box and such...
                 clearInterval(timeInterval);
             }
             
         })
+
+        if(timeLeft <= 0) {
+            quizEl.removeChild(questionsEl);
+            quizEl.removeChild(buttons);
+            clearInterval(timeInterval);
+            highScore();
+
+        }
 
         button1.addEventListener("click", function(){
             if (button1.textContent !== questions[i].answer) {
@@ -162,7 +183,66 @@ function quizFun(){
             }
         })
 
-        
+        function highScore() {
+
+            let finalScore = document.createElement("h6");
+            let scoreInput = document.createElement("input");
+            let submitDiv = document.createElement("a");
+            let submitButton = document.createElement("button");
+
+            quizEl.appendChild(finalScore);
+            quizEl.appendChild(scoreInput);
+            quizEl.appendChild(submitDiv);
+            submitDiv.appendChild(submitButton);
+            //submitDiv.href = "highscores.html" //Was working before prevent default. Look into this.
+            countEl.style.display = "none";
+            
+            scoreInput.setAttribute("id", "score-input");
+            submitDiv.setAttribute("id", "submit-div");
+            
+            
+            h3El.textContent = "Quiz Complete!!!";
+            finalScore.textContent = "Your final score is: " + (timeLeft + 1);
+            scoreInput.placeholder = "Enter Initials here";
+            submitButton.textContent = "Submit";
+
+            submitButton.addEventListener("click", function(e){
+                e.preventDefault();
+                let input = document.querySelector("#score-input").value;
+                console.log(input);
+                localStorage.setItem("Username", input)
+                localStorage.setItem("Score", timeLeft.valueOf() + 1);
+                document.location.href = "highscores.html";
+                getStorage();
+                
+                function getStorage() {
+                    let values= [],
+                    keys = Object.keys(localStorage),
+                    i = keys.length;
+
+                while ( i-- ) {
+                    values.push( localStorage.getItem(keys[i]));
+                }
+                return values;
+                }
+                
+                console.log(values);
+                
+                // let scoreSub = {
+                //     initials: input.value.trim(),
+                //     score: timeLeft.value.trim()
+                // };
+
+
+                
+
+
+            })
+
+
+        }
+
+
         
         }
 
